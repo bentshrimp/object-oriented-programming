631dca4140e0217ea7bc098fdd5b62aef4430b33
bent_shrimp <park-jinwoo03@naver.com>
2022-10-27 13:07:10 +0900

    Lab07-3

---
+++ 20223081-Lab07-3/src/Student.java
@@ -0,0 +1,33 @@
+
+public class Student {
+
+	private String names;
+	private int scores;
+	/**
+	 * @return the names
+	 */
+	public String getNames() {
+		return names;
+	}
+	/**
+	 * @param names the names to set
+	 */
+	public void setNames(String names) {
+		this.names = names;
+	}
+	/**
+	 * @return the scores
+	 */
+	public int getScores() {
+		return scores;
+	}
+	/**
+	 * @param scores the scores to set
+	 */
+	public void setScores(int scores) {
+		this.scores = scores;
+	}
+	
+	
+
+}
